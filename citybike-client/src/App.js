6882ebbccec1bb1313ab67abc3c698380e83dfbd
refo44 <refo44@gmail.com>
2020-08-14 07:11:52 -0500

    proyecto finalizado

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+
+
+export  const bikeIcon = new Icon({
+  iconUrl: '/assets/bikeIcon.svg',
+  iconRetinaUrl: '/assets/bikeIcon.svg',
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40],
+  shadowUrl: '/assets/marker-shadow.png',
+  shadowSize: [29, 40],
+  shadowAnchor: [7, 40],
+});
 
 class App extends Component {
   constructor() {
@@ -9,22 +22,39 @@ class App extends Component {
     this.state = {
       response: false,
       endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      lat: 25.761681,
+      lng: -80.191788,
+      zoom:16
     };
 
   }
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-   
+    
+    socket.on("FromAPI", data => this.setState({ response: data }));
   }
+
   render() {
     const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
-    return (
+    const position = [this.state.lat, this.state.lng];
 
+    const markers = response ? response.map( (station, key) => {
+  
+      return(<Marker position={[station.latitude, station.longitude]} icon={bikeIcon} key={station.id} >
+          <Popup>
+            <h3>{station.name}</h3>
+            <p>{station.extra.address}</p>
+            <ul>
+              <li> lat: {station.latitude} lon: {station.longitude} </li>
+              <li>  free bikes: {station.free_bikes} </li>
+            </ul>              
+          </Popup>
+       </Marker>);  
+    }) : '';
+
+     
+    return (
       <div className="map">
         <h1> City Bikes in Miami </h1>
         <Map center={position} zoom={this.state.zoom}>
@@ -32,6 +62,9 @@ class App extends Component {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+      
+                { markers }
+        
         </Map>
       </div>
     );
--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const axios = require("axios");
 const http = require("http");
 const socketIo = require("socket.io");
-const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
+const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach?fields=stations"
 
 
 
@@ -20,11 +21,31 @@ io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
   console.log('New connection ' + socketId + ' from ' + clientIp);
+  // clear interval from old connections
+   if (interval) {
+    clearInterval(interval);
+  }
+
+  // every second get data from api and emit response
+  interval = setInterval( () => getApiAndEmit(socket), 1000);
+  
   socket.on("disconnect", () => {
     console.log("Client disconnected");
+    clearInterval(interval);
   });
 });
 
+const getApiAndEmit =  (socket) => {
+  
+  axios.get(citybikeurl)
+  .then(response => {
+      let data = response.data.network.stations;
+      socket.emit('FromAPI', data );
+  })
+  .catch(error => {
+    console.log(error);
+  }); 
+};
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
